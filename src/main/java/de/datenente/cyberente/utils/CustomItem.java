a1bf50f08e55a664d7caa632e9aed7eb2edbfd7e
Paul Rakutt <keksgauner@gmail.com>
2025-11-05 18:00:02 +0100

    refactor: custom item

--- src/main/java/de/datenente/cyberente/utils/CustomItem.java
+++ src/main/java/de/datenente/cyberente/utils/CustomItem.java
@@ -50,19 +50,20 @@ public class CustomItem {
     ItemMeta itemMeta;
 
     /**
-     * Get a message from the MiniMessage
-     * @param message the message
-     * @return the message
+     * Translates a MiniMessage formatted string into a Component.
+     *
+     * @param text the MiniMessage formatted string to translate
+     * @return the translated Component
      */
-    public static Component text(String message) {
-        return MiniMessage.miniMessage().deserialize(message);
+    public static Component text(final String text) {
+        return MiniMessage.miniMessage().deserialize(text);
     }
 
-    public static CustomItem of(Material material) {
+    public static CustomItem of(final Material material) {
         return new CustomItem(material);
     }
 
-    public CustomItem(Material material) {
+    public CustomItem(final Material material) {
         this.itemFactory = Bukkit.getItemFactory();
         this.itemStack = new ItemStack(material);
 
@@ -83,31 +84,31 @@ public class CustomItem {
         return this.getItemStack();
     }
 
-    public CustomItem displayName(Component component) {
+    public CustomItem displayName(final Component component) {
         this.getItemMeta().displayName(component);
         return this;
     }
 
-    public CustomItem displayName(String name) {
+    public CustomItem displayName(final String name) {
         this.displayName(text(name));
         return this;
     }
 
-    public CustomItem lore(Component... lore) {
-        List<Component> loreList = new ArrayList<>(Arrays.asList(lore));
+    public CustomItem lore(final Component... lore) {
+        final List<Component> loreList = new ArrayList<>(Arrays.asList(lore));
         this.getItemMeta().lore(loreList);
         return this;
     }
 
-    public CustomItem lore(String... lore) {
-        List<Component> loreList = new ArrayList<>();
+    public CustomItem lore(final String... lore) {
+        final List<Component> loreList = new ArrayList<>();
         Arrays.stream(lore).forEach(l -> loreList.add(text(l)));
         this.getItemMeta().lore(loreList);
         return this;
     }
 
-    public CustomItem itemModel(String key) {
-        String[] splitKey = key.split(":");
+    public CustomItem itemModel(final String key) {
+        final String[] splitKey = key.split(":");
         if (splitKey.length == 2) {
             this.getItemMeta().setItemModel(new NamespacedKey(splitKey[0], splitKey[1]));
             return this;
@@ -115,98 +116,98 @@ public class CustomItem {
         throw new IllegalStateException("Invalid item model key: " + key);
     }
 
-    public CustomItem itemModel(NamespacedKey namespacedKey) {
+    public CustomItem itemModel(final NamespacedKey namespacedKey) {
         this.getItemMeta().setItemModel(namespacedKey);
 
         return this;
     }
 
     @Deprecated(since = "1.21.5")
-    public CustomItem customModelData(int id) {
+    public CustomItem customModelData(final int id) {
         this.getItemMeta().setCustomModelData(id);
         return this;
     }
 
-    public CustomItem unbreakable(boolean unbreakable) {
+    public CustomItem unbreakable(final boolean unbreakable) {
         this.getItemMeta().setUnbreakable(unbreakable);
         return this;
     }
 
-    public CustomItem addEnchantment(Enchantment enchantment, int level) {
+    public CustomItem addEnchantment(final Enchantment enchantment, final int level) {
         this.getItemMeta().addEnchant(enchantment, level, false);
         return this;
     }
 
-    public CustomItem addUnsafeEnchantment(Enchantment enchantment, int level) {
+    public CustomItem addUnsafeEnchantment(final Enchantment enchantment, final int level) {
         this.getItemMeta().addEnchant(enchantment, level, true);
         return this;
     }
 
-    public CustomItem removeEnchantment(Enchantment enchantment) {
+    public CustomItem removeEnchantment(final Enchantment enchantment) {
         this.getItemMeta().removeEnchant(enchantment);
         return this;
     }
 
     public boolean isPlayerHead() {
-        ItemStack itemStack = this.getItemStack();
+        final ItemStack itemStack = this.getItemStack();
         return itemStack.getType() == Material.PLAYER_HEAD || itemStack.getType() == Material.PLAYER_WALL_HEAD;
     }
 
-    public CustomItem owningPlayer(OfflinePlayer player) {
-        if (this.getItemMeta() instanceof SkullMeta skullMeta) {
+    public CustomItem owningPlayer(final OfflinePlayer player) {
+        if (this.getItemMeta() instanceof final SkullMeta skullMeta) {
             skullMeta.setOwningPlayer(player);
             return this;
         }
         throw new IllegalStateException("Item is not a player head");
     }
 
-    public CustomItem amount(int size) {
+    public CustomItem amount(final int size) {
         this.getItemStack().setAmount(size);
         return this;
     }
 
-    public CustomItem durability(int durability) {
-        if (this.getItemMeta() instanceof Damageable damageable) {
+    public CustomItem durability(final int durability) {
+        if (this.getItemMeta() instanceof final Damageable damageable) {
             damageable.setDamage((short) durability);
             return this;
         }
         throw new IllegalStateException("Item is not damageable");
     }
 
-    public CustomItem damage(int damage) {
-        if (this.getItemMeta() instanceof Damageable damageable) {
+    public CustomItem damage(final int damage) {
+        if (this.getItemMeta() instanceof final Damageable damageable) {
             damageable.setDamage(damage);
             return this;
         }
         throw new IllegalStateException("Item is not damageable");
     }
 
-    public CustomItem addDamage(int damage) {
-        if (this.getItemMeta() instanceof Damageable damageable) {
+    public CustomItem addDamage(final int damage) {
+        if (this.getItemMeta() instanceof final Damageable damageable) {
             damageable.setDamage(damageable.getDamage() + damage);
             return this;
         }
         throw new IllegalStateException("Item is not damageable");
     }
 
-    public CustomItem removeDamage(int damage) {
-        if (this.getItemMeta() instanceof Damageable damageable) {
+    public CustomItem removeDamage(final int damage) {
+        if (this.getItemMeta() instanceof final Damageable damageable) {
             damageable.setDamage(damageable.getDamage() - damage);
             return this;
         }
         throw new IllegalStateException("Item is not damageable");
     }
 
-    public CustomItem potionType(PotionType potionType) {
-        if (this.getItemMeta() instanceof PotionMeta potionMeta) {
+    public CustomItem potionType(final PotionType potionType) {
+        if (this.getItemMeta() instanceof final PotionMeta potionMeta) {
             potionMeta.setBasePotionType(potionType);
             return this;
         }
         throw new IllegalStateException("Item is not a potion");
     }
 
-    public CustomItem addCustomPotionEffect(PotionEffect potionEffect, boolean overwrite) {
-        if (this.getItemMeta() instanceof PotionMeta potionMeta) {
+    public CustomItem addCustomPotionEffect(final PotionEffect potionEffect, final boolean overwrite) {
+        if (this.getItemMeta() instanceof final PotionMeta potionMeta) {
             potionMeta.addCustomEffect(potionEffect, overwrite);
             return this;
         }
@@ -214,7 +215,7 @@ public class CustomItem {
     }
 
     public CustomItem removeCustomPotionEffect(PotionEffectType potionEffectType) {
-        if (this.getItemMeta() instanceof PotionMeta potionMeta) {
+        if (this.getItemMeta() instanceof final PotionMeta potionMeta) {
             potionMeta.removeCustomEffect(potionEffectType);
             return this;
         }
@@ -222,25 +223,25 @@ public class CustomItem {
     }
 
     public CustomItem clearCustomPotionEffects() {
-        if (this.getItemMeta() instanceof PotionMeta potionMeta) {
+        if (this.getItemMeta() instanceof final PotionMeta potionMeta) {
             potionMeta.clearCustomEffects();
             return this;
         }
         throw new IllegalStateException("Item is not a potion");
     }
 
-    public CustomItem addItemFlags(ItemFlag... itemFlags) {
+    public CustomItem addItemFlags(final ItemFlag... itemFlags) {
         this.getItemMeta().addItemFlags(itemFlags);
         return this;
     }
 
-    public CustomItem hasItemFlag(ItemFlag itemFlag) {
+    public CustomItem hasItemFlag(final ItemFlag itemFlag) {
         this.getItemMeta().hasItemFlag(itemFlag);
         return this;
     }
 
     public boolean isLeatherArmor() {
-        Material material = this.getItemStack().getType();
+        final Material material = this.getItemStack().getType();
         return (material == Material.LEATHER_HELMET
                 || material == Material.LEATHER_CHESTPLATE
                 || material == Material.LEATHER_LEGGINGS
@@ -248,7 +249,7 @@ public class CustomItem {
     }
 
     public boolean isIronArmor() {
-        Material material = this.getItemStack().getType();
+        final Material material = this.getItemStack().getType();
         return (material == Material.IRON_HELMET
                 || material == Material.IRON_CHESTPLATE
                 || material == Material.IRON_LEGGINGS
@@ -256,7 +257,7 @@ public class CustomItem {
     }
 
     public boolean isGoldenArmor() {
-        Material material = this.getItemStack().getType();
+        final Material material = this.getItemStack().getType();
         return (material == Material.GOLDEN_HELMET
                 || material == Material.GOLDEN_CHESTPLATE
                 || material == Material.GOLDEN_LEGGINGS
@@ -264,7 +265,7 @@ public class CustomItem {
     }
 
     public boolean isChainmailArmor() {
-        Material material = this.getItemStack().getType();
+        final Material material = this.getItemStack().getType();
         return (material == Material.CHAINMAIL_HELMET
                 || material == Material.CHAINMAIL_CHESTPLATE
                 || material == Material.CHAINMAIL_LEGGINGS
@@ -272,7 +273,7 @@ public class CustomItem {
     }
 
     public boolean isDiamondArmor() {
-        Material material = this.getItemStack().getType();
+        final Material material = this.getItemStack().getType();
         return (material == Material.DIAMOND_HELMET
                 || material == Material.DIAMOND_CHESTPLATE
                 || material == Material.DIAMOND_LEGGINGS
@@ -280,14 +281,14 @@ public class CustomItem {
     }
 
     public boolean isNetheriteArmor() {
-        Material material = this.getItemStack().getType();
+        final Material material = this.getItemStack().getType();
         return (material == Material.NETHERITE_HELMET
                 || material == Material.NETHERITE_CHESTPLATE
                 || material == Material.NETHERITE_LEGGINGS
                 || material == Material.NETHERITE_BOOTS);
     }
 
-    public CustomItem armorColor(Color color) {
+    public CustomItem armorColor(final Color color) {
         if (this.getItemMeta() instanceof LeatherArmorMeta leatherArmorMeta) {
             leatherArmorMeta.setColor(color);
             return this;
@@ -296,46 +297,46 @@ public class CustomItem {
     }
 
     public CustomItem resetArmorColor() {
-        if (this.getItemMeta() instanceof LeatherArmorMeta leatherArmorMeta) {
+        if (this.getItemMeta() instanceof final LeatherArmorMeta leatherArmorMeta) {
             leatherArmorMeta.setColor(this.getItemFactory().getDefaultLeatherColor());
             return this;
         }
         throw new IllegalStateException("Item is not leather armor");
     }
 
-    public CustomItem fireworkPower(int power) {
-        if (this.getItemMeta() instanceof FireworkMeta fireworkMeta) {
+    public CustomItem fireworkPower(final int power) {
+        if (this.getItemMeta() instanceof final FireworkMeta fireworkMeta) {
             fireworkMeta.setPower(power);
             return this;
         }
         throw new IllegalStateException("Item is not a firework");
     }
 
-    public CustomItem removeFireworkEffect(int index) {
-        if (this.getItemMeta() instanceof FireworkMeta fireworkMeta) {
+    public CustomItem removeFireworkEffect(final int index) {
+        if (this.getItemMeta() instanceof final FireworkMeta fireworkMeta) {
             fireworkMeta.removeEffect(index);
             return this;
         }
         throw new IllegalStateException("Item is not a firework");
     }
 
-    public CustomItem addFireworkEffects(FireworkEffect... effect) {
-        if (this.getItemMeta() instanceof FireworkMeta fireworkMeta) {
+    public CustomItem addFireworkEffects(final FireworkEffect... effect) {
+        if (this.getItemMeta() instanceof final FireworkMeta fireworkMeta) {
             fireworkMeta.addEffects(effect);
             return this;
         }
         throw new IllegalStateException("Item is not a firework");
     }
 
-    public CustomItem chargeEffect(FireworkEffect effect) {
-        if (this.getItemMeta() instanceof FireworkEffectMeta fireworkEffectMeta) {
+    public CustomItem chargeEffect(final FireworkEffect effect) {
+        if (this.getItemMeta() instanceof final FireworkEffectMeta fireworkEffectMeta) {
             fireworkEffectMeta.setEffect(effect);
             return this;
         }
         throw new IllegalStateException("Item is not a firework effect");
     }
 
-    public CustomItem setBannerPattern(int i, Pattern pattern) {
+    public CustomItem setBannerPattern(final int i, final Pattern pattern) {
         if (this.getItemMeta() instanceof BannerMeta bannerMeta) {
             bannerMeta.setPattern(i, pattern);
             return this;
@@ -343,7 +344,7 @@ public class CustomItem {
         throw new IllegalStateException("Item is not a banner");
     }
 
-    public CustomItem setBannerPatterns(List<Pattern> patterns) {
+    public CustomItem setBannerPatterns(final List<Pattern> patterns) {
         if (this.getItemMeta() instanceof BannerMeta bannerMeta) {
             bannerMeta.setPatterns(patterns);
             return this;
@@ -351,24 +352,24 @@ public class CustomItem {
         throw new IllegalStateException("Item is not a banner");
     }
 
-    public CustomItem removeBannerPattern(int i) {
-        if (this.getItemMeta() instanceof BannerMeta bannerMeta) {
+    public CustomItem removeBannerPattern(final int i) {
+        if (this.getItemMeta() instanceof final BannerMeta bannerMeta) {
             bannerMeta.removePattern(i);
             return this;
         }
         throw new IllegalStateException("Item is not a banner");
     }
 
-    public CustomItem addBannerPattern(Pattern pattern) {
-        if (this.getItemMeta() instanceof BannerMeta bannerMeta) {
+    public CustomItem addBannerPattern(final Pattern pattern) {
+        if (this.getItemMeta() instanceof final BannerMeta bannerMeta) {
             bannerMeta.addPattern(pattern);
             return this;
         }
         throw new IllegalStateException("Item is not a banner");
     }
 
-    public CustomItem addBannerPatterns(Pattern... patterns) {
-        if (this.getItemMeta() instanceof BannerMeta bannerMeta) {
+    public CustomItem addBannerPatterns(final Pattern... patterns) {
+        if (this.getItemMeta() instanceof final BannerMeta bannerMeta) {
             for (Pattern pattern : patterns) {
                 bannerMeta.addPattern(pattern);
             }
@@ -377,40 +378,40 @@ public class CustomItem {
         throw new IllegalStateException("Item is not a banner");
     }
 
-    public CustomItem addBookPage(String... pages) {
-        if (this.getItemMeta() instanceof WritableBookMeta writableBookMeta) {
+    public CustomItem addBookPage(final String... pages) {
+        if (this.getItemMeta() instanceof final WritableBookMeta writableBookMeta) {
             writableBookMeta.addPage(pages);
             return this;
         }
         throw new IllegalStateException("Item is not a book");
     }
 
-    public CustomItem bookAuthor(String author) {
-        if (this.getItemMeta() instanceof BookMeta bookMeta) {
+    public CustomItem bookAuthor(final String author) {
+        if (this.getItemMeta() instanceof final BookMeta bookMeta) {
             bookMeta.setAuthor(author);
             return this;
         }
         throw new IllegalStateException("Item is not a book");
     }
 
-    public CustomItem setBookPage(int page, String data) {
-        if (this.getItemMeta() instanceof WritableBookMeta writableBookMeta) {
+    public CustomItem setBookPage(final int page, final String data) {
+        if (this.getItemMeta() instanceof final WritableBookMeta writableBookMeta) {
             writableBookMeta.setPage(page, data);
             return this;
         }
         throw new IllegalStateException("Item is not a book");
     }
 
-    public CustomItem bookTitle(String title) {
-        if (this.getItemMeta() instanceof BookMeta bookMeta) {
+    public CustomItem bookTitle(final String title) {
+        if (this.getItemMeta() instanceof final BookMeta bookMeta) {
             bookMeta.setTitle(title);
             return this;
         }
         throw new IllegalStateException("Item is not a book");
     }
 
-    public CustomItem setBookPages(String... pages) {
-        if (this.getItemMeta() instanceof WritableBookMeta writableBookMeta) {
+    public CustomItem setBookPages(final String... pages) {
+        if (this.getItemMeta() instanceof final WritableBookMeta writableBookMeta) {
             writableBookMeta.setPages(pages);
             return this;
         }
